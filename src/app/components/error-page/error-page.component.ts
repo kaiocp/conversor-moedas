e87ab77bde749686885d86fe59c0de4cf0f344d9
kaiocp <kaiocp7@gmail.com>
2022-06-29 15:34:13 -0300

    refactor: error page

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error-page',
-  template: '<p>Página não encontrada.</p>',
-  styleUrls: ['./error-page.component.css']
+  template: `
+    <section class="d-flex flex-column justify-content-center align-items-center text-center">
+      <div class="d-flex justify-content-center align-items-center">
+        <img src="../../../assets/images/404.svg" alt="Error 404">
+        <h1>Página não encontrada.</h1>
+      </div>
+      <h4>Ir para <a routerLink="/">home</a></h4>
+    </section>
+  `,
+  styles: [`
+    * {
+      font-family: var(--font-karla)!important;
+      margin: 0 0!important;
+    }
+    section {
+      margin-top: 10rem!important;
+    }
+    img {
+      max-width: 30px;
+      margin-right: 10px!important;
+    }
+    div {
+      width: 100%;
+      margin: 0 auto;
+    }
+  `]
 })
-export class ErrorPageComponent implements OnInit {
-
+export class ErrorPageComponent {
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
